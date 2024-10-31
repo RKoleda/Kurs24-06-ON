dd2ec0c1d64bb367a85798a9b5a11b5f7581a4ef
RKoleda <R.Koleda@gmx.de>
2024-10-31 13:32:41 +0100

    Aenderungen gepushed

--- Nodeserver/server.mjs
+++ Nodeserver/server.mjs
@@ -1,9 +1,10 @@
 // server.mjs
 import { createServer } from 'node:http';
+import { getRandomQuote } from 'inspirational-quotes';
 
 const server = createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('I want waffle fries\n');
+  res.end(getRandomQuote());
 });
 
 console.log(typeof (server));
